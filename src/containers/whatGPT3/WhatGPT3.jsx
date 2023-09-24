4b9cceb299644c3808a7f389ca71ec5868fd5dd9
Nayo <axelbalse@gmail.com>
2023-09-25 03:01:52 +0530

    First commit

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Feature from '../../components/feature/Feature';
+import './whatGPT3.css';
+
+const WhatGPT3 = () => (
+  <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+    <div className="gpt3__whatgpt3-feature">
+      <Feature title="What is GPT-3" text="
+GPT-3 (Generative Pre-trained Transformer 3) is a language model developed by OpenAI. It uses deep learning techniques to generate human-like text based on input prompts. With 175 billion parameters, it is one of the most powerful and versatile language models created to date." />
+    </div>
+    <div className="gpt3__whatgpt3-heading">
+      <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+      <p>Explore the Library</p>
+    </div>
+    <div className="gpt3__whatgpt3-container">
+      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+    </div>
+  </div>
+);
+
+export default WhatGPT3;
